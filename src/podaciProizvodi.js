305e0362284ef5aeefa49e67833a4a18d605f7f1
Isidora Maric <isidoramaricpriboj@gmail.com>
2023-09-01 20:14:28 +0200

    Napravljen niz proizvoda koje poziva App.js

---
+++ src/podaciProizvodi.js
@@ -0,0 +1,87 @@
+import React from "react";
+
+const proizvodiPodaci = [
+    {
+        id: 1,
+        naziv: "AHA gel serum 40%",
+        opis:
+            "Gel serum 40 % sa voćnim kiselinama namenjen isključivo profesionalnoj upotrebi u kozmetičkim salonima.",
+        ukupno: 0,
+        cena: 2500,
+        url: "https://primakozmetika.com/wp-content/webp-express/webp-images/uploads/2019/04/20201207-AHA-40.jpg.webp",
+    },
+    {
+        id: 2,
+        naziv: "AHA Neutralizator",
+        opis:
+            "Specijalno formulisan proizvod koji zaustavlja proces pilinga voćnim kiselinama i potpuno ih neutrališe.",
+        ukupno: 0,
+        cena: 3000,
+        url: "https://primakozmetika.com/wp-content/webp-express/webp-images/uploads/2019/04/neutralizator.jpg.webp",
+    },
+    {
+        id: 3,
+        naziv: "Hydro Force antirid krema",
+        opis:
+            "Antirid gel krema sa hijaluronskom kiselinom je laka gel podloga, veoma pogodna za nežnu kožu oko očiju.",
+        ukupno: 0,
+        cena: 2400,
+        url: "https://primakozmetika.com/wp-content/webp-express/webp-images/uploads/2020/11/Hydro-force-krema-antirid-700x700.png.webp",
+    },
+    {
+        id: 4,
+        naziv: "Derma Calm krema za lice za osetljivu kožu",
+        opis:
+            "Nežna krema za lice sa hijaluronom i bisabololom, aloe verom i shea buterom, pomaže regeneraciju i revitalizaciju osetljive, iritirane i umorne kože.",
+        ukupno: 0,
+        cena: 1200,
+        url: "https://primakozmetika.com/wp-content/webp-express/webp-images/uploads/2019/12/Derma-calm-krema-700x700.png.webp",
+    },
+    {
+        id: 5,
+        naziv: "Skin Rescue noćna krema za lice za problematičnu kožu",
+        opis:
+            "Noćna krema za lice sa ihtiolom i niacinamidom nemasne formulacije namenjena je masnoj i aknoznoj koži lica. ",
+        ukupno: 0,
+        cena: 3500,
+        url: "https://primakozmetika.com/wp-content/webp-express/webp-images/uploads/2019/04/Skin-rescue-nocna-krema-700x700.png.webp",
+    },
+    {
+        id: 6,
+        naziv: "PAPAYA + Hydrating Complex piling serum",
+        opis:
+            "Piling serum za lice sa enzimom papaje obezbeđuje dugotrajnu hidrataciju i najbolju pripremu lica za nanošenje aktivnih sastojaka u nastavku nege.",
+        ukupno: 0,
+        cena: 4000,
+        url: "https://primakozmetika.com/wp-content/webp-express/webp-images/uploads/2020/09/papaja-700x700.jpg.webp",
+    },
+    {
+        id: 7,
+        naziv: "Oil Control Losion za čišćenje lica",
+        opis:
+            "Losion za čišćenje lica namenjen masnoj i mešovitoj koži.",
+        ukupno: 0,
+        cena: 1500,
+        url: "https://primakozmetika.com/wp-content/webp-express/webp-images/uploads/2020/08/Oil-control-losion-700x700.png.webp",
+    },
+    {
+        id: 8,
+        naziv: "ALOE VERA serum",
+        opis:
+            "Serum za lice idealan izbor za osetljivu kožu sklonu alergijama.",
+        ukupno: 0,
+        cena: 3000,
+        url: "https://primakozmetika.com/wp-content/webp-express/webp-images/uploads/2020/09/aloe-vera-700x700.jpg.webp",
+    },
+    {
+        id: 9,
+        naziv: "PAPAYA + Hydrating Complex piling serum",
+        opis:
+            "Piling serum za lice sa enzimom papaje obezbeđuje dugotrajnu hidrataciju.",
+        ukupno: 0,
+        cena: 2800,
+        url: "https://primakozmetika.com/wp-content/webp-express/webp-images/uploads/2020/09/papaja-700x700.jpg.webp",
+    },
+];
+
+export default proizvodiPodaci;
